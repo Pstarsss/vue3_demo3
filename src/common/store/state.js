f02f8b17b9ed8ce152e2ad6903bb9037f8dca583
panxing <xingpan@yangqianguan.com>
2021-10-26 11:12:28 +0800

    eee

--- src/common/store/state.js
+++ src/common/store/state.js
@@ -1,6 +1,11 @@
 /*
  * @Author: xingpan 
  * @Date: 2021-10-22 16:43:57 
- * @Last Modified by:   xingpan 
- * @Last Modified time: 2021-10-22 16:43:57 
+ * @Last Modified by: xingpan
+ * @Last Modified time: 2021-10-26 10:55:39
  */
+
+export default {
+    user: {},
+    fetched: false
+}
