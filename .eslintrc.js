ed63945ca4e17547381fca47814127cf24569119
panxing <xingpan@fintopia.tech>
2022-12-27 15:52:15 +0800

    fix useless file - 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,10 +13,6 @@ module.exports = {
         "@vue/prettier/@typescript-eslint",
     ],
     
-    parserOptions: {
-        ecmaVersion: 2020,
-    },
-
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
--- webpack.config.js
+++ webpack.config.js
@@ -3,49 +3,44 @@ import path from "path";
 const resolvePath = (dir) => path.resolve(__dirname, dir);
 
 module.exports = {
-  entry: "./src/main.ts",
+    entry: "./src/main.ts",
 
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-
-  devServer: {
-    proxy: {
-      "/api": {
-        target: "https://loan-admin-test.yangqianguan.com",
-        changeOrigin: true,
-      },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
     },
-  },
 
-  resolve: {
-    alias: {
-      "@": resolvePath("src"),
-      "px-admin": "./src",
+    devServer: {
     },
-    //因import引入的vue文件都没有加.vue后缀导致报404，所以加了这个配置
-    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
-  },
 
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
-      { test: /\.vue$/, use: ["vue-loader"] },
-      {
-        test: /\.css$/i,
-        use: ["vue-style-loader", "style-loader", "css-loader"],
-        options: {
-          import: true,
+    resolve: {
+        alias: {
+            "@": resolvePath("src"),
+            "px-admin": "./src",
         },
-      },
-      {
-        test: /\.less$/i,
-        use: ["vue-style-loader", "style-loader", "less-loader"],
-        options: {
-          import: true,
-        },
-      },
-    ],
-  },
+
+        //因import引入的vue文件都没有加.vue后缀导致报404，所以加了这个配置
+        extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+    },
+
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
+            { test: /\.vue$/, use: ["vue-loader"] },
+            {
+                test: /\.css$/i,
+                use: ["vue-style-loader", "style-loader", "css-loader"],
+                options: {
+                    import: true,
+                }
+            },
+            {
+                test: /\.less$/i,
+                use: ["vue-style-loader", "style-loader", "less-loader"],
+                options: {
+                    import: true,
+                }
+            }
+        ],
+    },
 };
